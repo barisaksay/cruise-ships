ca2b9d59a7d0d29d7795c98a0b7cd6c07eb77293
baris_aksay <barish.aksay@gmail.com>
2025-01-17 16:32:14 +0100

    added test for startingPort prop.Polished other tests

--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -1,17 +1,21 @@
 const Ship = require("../ship")
 
 describe('Ship constructor', () => { 
+        let magna= new Ship('Nice')
 
     it('Ship class can be instantiated',()=>{
-        expect(new Ship()).toBeInstanceOf(Ship)
+        expect(magna).toBeInstanceOf(Object)
+    })
+
+    it('Ship has a starting port',()=>{
+        expect(magna.startingPort).toBe('Nice')
     })
 
     it('Ship class have passengers prop set to 0',()=>{
-        expect(new Ship()).toHaveProperty('passengers',0)
+        expect(magna).toHaveProperty('passengers',0)
     })
 
     it('addPassengers should increase passengers prop with given arg',()=>{
-        let magna= new Ship('Nice')
         magna.addPassengers(3)
         expect(magna.passengers).toBe(3)
     })
